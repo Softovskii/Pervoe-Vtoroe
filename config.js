c0f50d26748ae284cf5ecdd979fa8a0be8034d0d
Softovskii <zovtovskii@gmail.com>
2026-05-09 19:43:34 +0600

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 const CONFIG = {
     whatsappNumber: "77064069814",
-    csvUrl: "https://docs.google.com/spreadsheets/d/e/2PACX-1vT3joTj2hKkKgH3WJ0tMISWQeXtbb7aPA85HMAqV54F1gWPp-HNWPJ1TjegOGq85E6eFBKcjl0X_cXN/pub?output=csv"
+    csvUrl: "https://docs.google.com/spreadsheets/d/e/2PACX-1vT3joTj2hKkKgH3WJ0tMISWQeXtbb7aPA85HMAqV54F1gWPp-HNWPJ1TjegOg85E6eFBKcj10X_cXN/pub?output=csv"
 };
